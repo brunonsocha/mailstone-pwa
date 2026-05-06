7cc42fea6902f26fb7f0bf199e03acc110a9f67b
brunonsocha <brunon.socha@gmail.com>
2026-05-06 19:46:41 +0200

    lets go

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrDBXsNBc2RV1rRhnB8GmY1_TmifXsQzQ",
+  authDomain: "orangesoapstone.firebaseapp.com",
+  projectId: "orangesoapstone",
+  storageBucket: "orangesoapstone.firebasestorage.app",
+  messagingSenderId: "630637970302",
+  appId: "1:630637970302:web:035dfde9516c4f19a8292f"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
